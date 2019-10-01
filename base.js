53b2499f69278ba09bcb3ee573eb9d3a779bd52c
Alexandre Machado <amch.smd@gmail.com>
2019-10-01 16:36:08 -0600

    Fazendo alguma coisa, pelo menos
    Funções de mapeamento e definição de propriedades.

--- base.js
+++ base.js
@@ -10,6 +10,12 @@ const getLast = arr => arr.slice(0, arr.length - 1);
 /**  @function randomize {Object} - Função que retorna um valor aleatório entre dois valores */
 const randomize = min => max => Math.floor(Math.random() * max) + min;
 
+/**  @function map {Object} - Função de mapeamento do array da cobra */
+const map = o1 => o2 => o2.map(o1);
+
+/**  @function prop {Object} - Função que retorna se há uma certa propriedade num objeto */
+const prop = k => o => o[k];
+
 /** 
 	* @todo Estabelecer funções base do jogo
 	* @body Já criei algumas funções que podem ser úteis pra lógica base do jogo, mas com certeza tem muito mais coisa.
